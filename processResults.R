2d23b0f29b0cd863721d66dd77eb254d99171159
Alex Franks <afranks@rclogin11.rc.fas.harvard.edu>
2016-03-24 17:30:46 -0400

    processResults

--- processResults.R
+++ processResults.R
@@ -1,16 +1,19 @@
-directory <- "/n/airoldifs2/lab/afranks/shared_subspace"
-lossList <- list()
+ directory <- "/n/airoldifs2/lab/afranks/shared_subspace"
+lossList <- allLosses <- list()
 for(i in 1:3) {
-  files <- list.files(directory, pattern=sprintf("vmode-%i",i))
+  files <- list.files(directory, pattern=sprintf("srtest-%i",i))
   count <- 0
   lossVecMean <- rep(0, 3)
+  lossMat <- matrix(NA, nrow=0, ncol=3)
   for(f in files) {
     print(f)
     count <- count+1
     load(sprintf("%s/%s", directory, f))
     print(resList[[1]]$S)
     lossVecMean <- lossVecMean + lossVec
+    lossMat <- rbind(lossMat, lossVec)
   }
   lossList[[i]] <- lossVecMean / count
-
+  allLosses[[i]] <- lossMat
 }
+apply(allLosses[[3]], 2, function(x) quantile(x, c(0.1, 0.9)))
