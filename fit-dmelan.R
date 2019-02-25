f34d68b731c92ea56801f9c4e99fa7ec9f5795b8
Alexander Franks <afranks@gmail.com>
2019-02-25 07:49:56 -0800

    Updated dmelan fit

--- fit-dmelan.R
+++ fit-dmelan.R
@@ -1,16 +1,13 @@
 rm(list=ls())
 
+library(tidyverse)
 library(rstiefel)
-source("helper.R")
+library(colorspace)
+source("subspace-functions.R")
 source("fit-subspace.R")
 ##load("ae_XY_m527.RData")
-load("C18_all.RData")
+load("results/C18_all.RData")
 
-## number of eigenvectors if pooled space
-S <- 12
-## number of eigenvectors in group subspace
-R <- 2
-P <- ncol(Y)
 
 table(X$Line)
 table(X$Sex, X$Line)
@@ -26,6 +23,9 @@ table(X$Age, X$Sex)
 
 XDM <- model.matrix(~ -1 + Age:Sex, data=X )
 ngroups <- ncol(XDM)
+
+group <- apply(XDM, 1, function(x) which(x==1))
+
 ## XDM_All <- model.matrix(~ -1 + Age + Sex + Line, data=X )
 ## XDM_Age <- model.matrix(~ -1 + Age, data=X )
 
@@ -46,36 +46,42 @@ for( i in 1:ngroups ) {
     nvec[i] <- nrow(res)
     
 }
-getRank(fit$residuals)
 
-## Vinit <- svd(do.call(cbind, lapply(1:ngroups, function(k) svd(t(residualList[[k]]))$u[, 1:R])))$u[, 1:min(ngroups*R, S)]
+S <- getRank(fit$residuals)
+R <- S
+P <- ncol(Y)
+
 Vinit <- svd(do.call(cbind, lapply(1:ngroups, function(k) svd(t(residualList[[k]]))$u[, 1:S])))$u[, 1:S]
 PrecVec = rep(5, ngroups)
-EMFit <- subspaceEM(Slist, P=P, S=S, R=R, nvec=nvec, Vstart=Vinit, verbose=TRUE, rho1=1e-5, rho2=1-1e-5)
+EMFit <- subspaceEM(residualList, P=P, S=S, R=R, nvec=nvec, Vstart=Vinit, verbose=TRUE, M_iters=1000)
 Vinit <- EMFit$V
 
-## In paper using, 04-28
+evalRatiosQuad <- compute_variance_explained(Vinit, residualList, nvec, 1/EMFit$PrecVec)
+rownames(evalRatiosQuad) <- colnames(XDM)
+colnames(evalRatiosQuad) <- 1:S
+tib <- as_tibble(evalRatiosQuad)
+tib$Type <- colnames(XDM)
+tib %>% gather(key=S, value=GF, -Type) %>%
+    ggplot(aes(x=as.numeric(S), y=GF, col=Type)) + geom_line(size=1.5) + scale_color_discrete_qualitative(alpha=0.75) +
+    xlab("Dimension") + ylab("Goodness of Fit") +
+    geom_hline(yintercept=1, linetype="dashed")
+
 save(EMFit, Vinit, S, R, P, nvec, residualList,
      file=sprintf("dmelanEM-%s.RData", format(Sys.Date(), "%m-%d")))
 
-evalRatios <- sapply(1:ngroups, function(k) {
-    numer <- sum(eigen(t(Vinit[, 1:S]) %*% Slist[[k]] %*% Vinit[, 1:S])$values)/nvec[k]
-    denom <- sum(svd(residualList[[k]])$d[1:S]^2)/nvec[k]
-    correction <-  (denom - 1/EMFit$PrecVec[k] * S * P / nvec[k])  / denom
-    (numer/denom) / correction
-})
-
 pdf(sprintf("paper/Figs/dmelanRatio-%s.pdf", format(Sys.Date(), "%m-%d")), font="Times")
 par(mar=c(6.1, 4.1, 4.1, 2.1))
-barplot(evalRatios, ylim=c(0, 1), xlab="", main="", space=0.2, cex.axis=1.5, col="#646464", col.axis="black", col.main="black", border=NA)
+barplot(evalRatiosQuad[, S], ylim=c(0, 1), xlab="", main="", space=0.2, cex.axis=1.5, col="#646464", col.axis="black", col.main="black", border=NA)
 text(seq(1, 7, length.out=ngroups) , par("usr")[1], labels=paste(rep(c("Young", "Middle", "Old"), 2), rep(c("F", "M"), each=ngroups/2), sep="/"),
      srt=45, xpd=TRUE, cex=1.5, col="black", adj=c(1, NA))
 dev.off()
 
+
+
 OmegaList <- Ulist <- list()
 for(k in 1:ngroups) {
 
-    eigK <- eigen(diag(S) - EMFit$PhiList[[k]] / EMFit$PrecVec[k])
+    eigK <- eigen( Matrix::nearPD(diag(S) - EMFit$PhiList[[k]] / EMFit$PrecVec[k])$mat)
 
     OmegaList[[k]] <- eigK$values
     Ulist[[k]] <- Vinit %*% eigK$vectors
@@ -84,53 +90,87 @@ for(k in 1:ngroups) {
 s2vec <- 1/EMFit$PrecVec
 
 initSS <- list(V=Vinit, Ulist=Ulist, OmegaList=OmegaList, s2vec=s2vec)
-samples <- fitSubspace(P, S, R, Q=S-R, Slist,
+samples <- fitSubspace(P, S, R, Q=S-R, residualList,
                    nvec, ngroups, init=initSS,
-                   niters=200, draw=c(V=FALSE))
+                   niters=10000, nskip=10)
+
+
+
 save(samples, initSS, file=sprintf("dmelanBayes-%s.RData",
                                    format(Sys.Date(), "%m-%d")))
 
+regionColors <- rep(sequential_hcl(palette="Heat", 3), 2)
+pch <- rep(c(19, 15), each=3)
 
-## Posterior plots
-pdf(sprintf("paper/Figs/dmelanPosterior-%s.pdf", format(Sys.Date(), "%m-%d")), font="Times")
-with(samples,
-     posteriorPlot(Osamps[1:2, 1:2, , ], omegaSamps[1:2, , ],
-                s2samps, nsamps=100, groups=1:ngroups,
-                probRegion=0.95, hline=NULL,
-                col=rep(1:3, 2), pch=rep(c(2, 3), each=3), lty=rep(c(1, 2), each=3), ymax=log(8), logRatio=TRUE, plotPoints=FALSE))
-## legend("topright", legend=paste(rep(c("Young", "Middle", "Old"), 2), rep(c("F", "M"), each=3), sep="/"), col=rep(1:3, 2), pch=rep(c(2, 3), each=3), bty="n")
-legend("topright", legend=paste(rep(c("Young", "Middle", "Old"), 2), rep(c("F", "M"), each=3), sep="/"), col=rep(1:3, 2), lty=rep(c(1, 2), each=3), bty="n", lwd=3)
-dev.off()
+g1 <- 1
+g2 <- 3
 
-for(k in 1:ngroups) {
+pmList <- lapply(1:ngroups, function(g) {
+    getPostMeanPsi(samples$Osamps[, , g , ],
+                   samples$omegaSamps[, g, ],
+                   samples$s2samps[g, ], 100)})
 
-    eigK <- eigen(solve(EMFit$PhiList[[k]][1:2, 1:2])*EMFit$PrecVec[k]-diag(2))
-    lambda <- eigK$values
-    evecs <- eigK$vectors
+view_indices <- c(5, 6)
 
-    maxIndex <- which.max(lambda)
-    lamRatio <- lambda[maxIndex]/lambda[-maxIndex]
-    angle <- atan(evecs[2, maxIndex]/evecs[1, maxIndex])
+## age difference
+O <- svd(pmList[[1]][1:R, 1:R] + pmList[[4]] -
+         pmList[[3]][1:R, 1:R] - pmList[[6]])$u[, view_indices]
 
-    points(angle, lamRatio, col=k, pch=19, cex=2)
-    print(sprintf("%f, %f", angle, lamRatio))
+## gender difference
+O <- svd(pmList[[1]][1:R, 1:R] + pmList[[2]] + pmList[[3]] -
+         pmList[[4]][1:R, 1:R] - pmList[[5]] - pmList[[6]])$u[, view_indices]
+
+
+Vstar <- Vinit[, 1:R] %*% O
+Osamps_proj <- array(dim = c(2, 2, ngroups, 1000))
+omegaSamps_proj <- array(dim = c(2, ngroups, 1000))
+
+for(i in 1:1000) {
+    for(k in 1:ngroups) {
+        ## Osamps2[1:R, 1:R, k, i ] <- t(O) %*% samples$Osamps[1:R, 1:R, k, i]
+
+        tmp <- t(O) %*% samples$Osamps[1:R, 1:R, k, i]
+        omega <- samples$omegaSamps[1:R, k, i]
+        eig <- eigen(tmp %*% diag(omega / (1-omega)) %*% t(tmp))
+        Osamps_proj[, , k, i] <- eig$vectors
+        lambda <- eig$values
+        omegaSamps_proj[, k, i] <- lambda/(lambda+1)
+    }
 }
 
+
+pdf(sprintf("paper/Figs/dmelanPosterior-%-i-%i-%s.pdf", g1, g2, format(Sys.Date(), "%m-%d")), font="Times")
+toshow <- 1:6
 with(samples,
-     vectorPlot(Osamps[c(3,7), c(3,7), , ], omegaSamps[c(3,6), , ],
-                s2samps, nsamps=100, groups=1:ngroups,
-                probRegion=0.95, type=2, hline=1))
+     posteriorPlot(Osamps_proj[, , , 501:1000], omegaSamps_proj[, , 501:1000],
+                s2samps[, 501:1000], nsamps=100, groups=toshow,
+                probRegion=0.95, hline=NULL, type="mag",
+                plotPoints=TRUE, col=regionColors, ymax=NULL, cex.pts=1, pch=pch))
 
-mag <- apply(EMFit$V[, 1:2], 1, function(x) sqrt(sum(x^2)))
-indices <- which(mag > quantile(mag, 0.95))
-plot(EMFit$V[indices, 1], EMFit$V[indices, 2], xlab="V1", ylab="V2", cex=0.5,
-     xlim=c(-0.1, 0.1), ylim=c(-0.1, 0.1))
-text(EMFit$V[indices, 1], EMFit$V[indices, 2], labels=rownames(EMFit$V)[indices],
-     xlab="V1", ylab="V2", cex=0.5, pos=4)
-abline(h=0, v=0)
+legend("topleft", legend=colnames(XDM), col=regionColors, pch=pch, title="Leukemia type", cex=1.2, bty="o", box.col="white")
 
-## Check magnitude of eigenvalues vs P/n
-eigVals <- sapply(1:ngroups, function(g) with(samples, head(rowMeans(omegaSamps[, g, ]/(1-omegaSamps[, g, ])), n=S)))
+omegaSamps_proj[, , 1000]/(1-omegaSamps_proj[, , 1000])
 
-rowMeans(sapply(1:R, function(i) (1 - sqrt(P/nvec)/eigVals[i, ]^2) / (1 + sqrt(P/nvec)/eigVals[i, ])))
-sapply(1:R, function(i) (1 - sqrt(P/(ngroups*min(nvec)))/min(eigVals[i, ])^2) / (1 + sqrt(P/(ngroups*min(nvec)))/min(eigVals[i, ])))
+
+    
+
+dev.off()
+
+preds <- makePrediction(fit$residuals, EMFit$V,
+                        samples$Osamps,
+                        samples$omegaSamps,
+                        samples$s2samps,
+               nvec=nvec, ngroups=length(nvec), nsamps=1000,
+               numToAvg=500)
+
+## For how many 
+sapply(1:6, function(type) {
+       mean(type == apply(preds, 1, which.max)[group == type])})
+mean(group == apply(preds, 1, which.max))
+
+
+t(apply(preds, 1, order, decreasing=TRUE))
+sort(preds,partial=6)[6]
+
+
+
